9b160596aeb40941628f77e58041eed1b348da94
Wannes Malfait <wannes.malfait@gmail.com>
2021-01-06 19:03:46 +0100

    Cleaned up thread code + benchmarking

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,44 @@
-use std::io;
+use std::cmp::max;
 use std::thread;
+use std::time::{Duration, Instant};
 
 mod hankel;
 use hankel::Hankel;
-
-fn test_for_cycles(offset: usize) -> io::Result<()> {
-    const MAX: usize = 2000;
-    // START should be bigger than 2*DECREMENT - 4
-    const START: usize = 20;
-    // How many vertices from the previous cycle
-    // do we change to try to create a new cycle?
-    // should be at least INCREMENT
-    const DECREMENT: usize = 8;
-    // 2 * the number of threads
-    const INCREMENT: usize = 8;
+/// Brute force search for Hamiltonian cycles
+///
+/// Searches for cycles of length n, where n goes from
+/// `start + offset` upto and including `maximum` in steps of `increment`.
+/// Since cycles of odd length are impossible, `start` and `increment` should always be
+/// even. If run with `m` threads, use `increment = 2 * m`.
+///
+/// `divisor` indicates where to start searching in the previous path. If the path is
+/// length `n` then we start a backtracking search from index `n/divisor`. Should not be 0
+fn test_for_cycles(maximum: usize, start: usize, increment: usize, offset: usize, divisor: usize) {
+    // When we try to create a new cycle
+    let decrement = max(6, increment);
     // Generate all the primes we need
-    let primes = hankel::gen_primes_upto_n(2 * MAX - 1);
+    let primes = hankel::gen_primes_upto_n(2 * maximum - 1);
     // Create the first Hamiltonian cycle
-    let mat = Hankel::prime_sum_matrix(START + offset, Some(&primes));
+    let mat = Hankel::prime_sum_matrix(start + offset, Some(&primes));
     let mut previous_path = mat
         .is_hamiltonian()
         .expect("No Hamiltonian cycle found for the starting index");
-    let mut i = START + offset;
-    'outer: while i <= MAX {
+    let mut i = start + offset;
+    let now = Instant::now();
+    while i <= maximum {
         let mat = Hankel::prime_sum_matrix(i, Some(&primes));
-        let mut start_index = i - DECREMENT;
         // We attempt to re-use the previous cycle by only changing the last
         // vertices in the cycle
-        while !mat.hamiltonian_cycle(&mut previous_path, start_index) {
-            // If it didn't work create a new cycle
-            if start_index <= i - DECREMENT * 2 + 4 {
-                println!("Thread: {} trying from 0", offset / 2);
-                if !mat.hamiltonian_cycle(&mut previous_path, 1) {
-                    break 'outer;
-                } else {
-                    break;
-                }
-            }
+        if !mat.hamiltonian_cycle(&mut previous_path, i - decrement) {
+            // It didn't work -> create a new cycle from scratch
             println!(
-                "Thread: {} failed with index {}, now trying with {}",
+                "Thread: {} creating cycle of length {} from scratch",
                 offset / 2,
-                start_index,
-                start_index - DECREMENT + 4
+                i
             );
-            start_index -= DECREMENT - 4;
+            if !mat.hamiltonian_cycle(&mut previous_path, i / divisor) {
+                break; // Didn't find a cycle
+            }
         }
         // Double check if it is actually a valid cycle
         if mat.valid_cycle(&previous_path) {
@@ -54,36 +49,34 @@ fn test_for_cycles(offset: usize) -> io::Result<()> {
         // If the even index has a cycle then we can always remove one vertex
         // to create a valid path of length index - 1. Therefore we only check
         // the even indices.
-        i += INCREMENT;
-        for _ in 0..INCREMENT {
+        i += increment;
+        for _ in 0..increment {
             previous_path.push(0);
         }
     }
-    Ok(())
+    println!("Thread: {} took {:?}", offset / 2, now.elapsed());
 }
-fn main() -> io::Result<()> {
+fn main() {
     const STACK_SIZE: usize = 1024 * 1024 * 4;
-    // Spawn thread with explicit stack size
+    const THREAD_NUM: usize = 6;
+    let max = 4000;
+    let start = 2000;
+    let increment = 2 * THREAD_NUM;
+    let divisor = 2; // Seems to give faster result, went from 54s -> 42s
+    let mut threads = Vec::with_capacity(THREAD_NUM);
+    // Spawn threads with explicit stack size
     // Needed because of the heavy recursion
-    let child = thread::Builder::new()
-        .stack_size(STACK_SIZE)
-        .spawn(|| test_for_cycles(0))
-        .unwrap();
-    let child2 = thread::Builder::new()
-        .stack_size(STACK_SIZE)
-        .spawn(|| test_for_cycles(2))
-        .unwrap();
-    let child3 = thread::Builder::new()
-        .stack_size(STACK_SIZE)
-        .spawn(|| test_for_cycles(4))
-        .unwrap();
-    let child4 = thread::Builder::new()
-        .stack_size(STACK_SIZE)
-        .spawn(|| test_for_cycles(6))
-        .unwrap();
-    // Wait for thread to join
-    child.join().unwrap();
-    child2.join().unwrap();
-    child3.join().unwrap();
-    child4.join().unwrap()
+    for i in 0..THREAD_NUM {
+        let builder = thread::Builder::new();
+        threads.push(
+            builder
+                .stack_size(STACK_SIZE)
+                .spawn(move || test_for_cycles(max, start, increment, i * 2, divisor))
+                .unwrap(),
+        );
+    }
+    // Wait for threads to join
+    for thread in threads {
+        let _ = thread.join();
+    }
 }
