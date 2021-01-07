c12bb19d6a4aed693abfd6a05b5af94a35a2aa58
Wannes Malfait <wannes.malfait@gmail.com>
2021-01-07 19:12:56 +0100

    Added command line options

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::cmp::max;
 use std::thread;
-use std::time::{Duration, Instant};
+use std::time::Instant;
 
 mod hankel;
 use hankel::Hankel;
+
+use clap::{App, Arg};
+
 /// Brute force search for Hamiltonian cycles
 ///
 /// Searches for cycles of length n, where n goes from
@@ -36,7 +39,11 @@ fn test_for_cycles(maximum: usize, start: usize, increment: usize, offset: usize
                 offset / 2,
                 i
             );
-            if !mat.hamiltonian_cycle(&mut previous_path, i / divisor) {
+            let cycles_start = match divisor {
+                0 => 1,
+                _ => i / divisor,
+            };
+            if !mat.hamiltonian_cycle(&mut previous_path, cycles_start) {
                 break; // Didn't find a cycle
             }
         }
@@ -57,21 +64,92 @@ fn test_for_cycles(maximum: usize, start: usize, increment: usize, offset: usize
     println!("Thread: {} took {:?}", offset / 2, now.elapsed());
 }
 fn main() {
-    const STACK_SIZE: usize = 1024 * 1024 * 4;
-    const THREAD_NUM: usize = 6;
-    let max = 4000;
-    let start = 2000;
-    let increment = 2 * THREAD_NUM;
-    let divisor = 2; // Seems to give faster result, went from 54s -> 42s
-    let mut threads = Vec::with_capacity(THREAD_NUM);
+    let matches = App::new("Prime sum sequences")
+        .version("1.0")
+        .author("Wannes M. <wannes.malfait@gmail.com>")
+        .about("Bruteforce search for prime sum sequences")
+        .arg(
+            Arg::with_name("Start")
+                .short("s")
+                .long("start")
+                .help("Start searching from this length")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("Maximum")
+                .short("m")
+                .long("max")
+                .help("Search upto and including Maximum")
+                .required(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("Thread num")
+                .short("t")
+                .long("threads")
+                .help("Number of threads")
+                .default_value("1")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("Stack size")
+                .long("stack")
+                .help("Stack size in bytes")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("Divisor")
+                .long("divisor")
+                .short("d")
+                .help("Greedily start at n/divisor if non-zero")
+                .default_value("0")
+                .takes_value(true),
+        )
+        .get_matches();
+    let stack_size;
+    if let Some(arg) = matches.value_of("Stack size") {
+        stack_size = arg.parse().expect("Expected a number");
+    } else {
+        stack_size = 1024 * 1024;
+    }
+    let thread_num = matches
+        .value_of("Thread num")
+        .unwrap()
+        .parse()
+        .expect("Expected a number");
+    let maximum = matches
+        .value_of("Maximum")
+        .unwrap()
+        .parse()
+        .expect("Expected a number");
+    let start;
+    if let Some(arg) = matches.value_of("Start") {
+        start = arg.parse().expect("Expected a number");
+        if start < thread_num * 2 {
+            eprintln!("The number of threads must be less than the start/2");
+            return;
+        }
+    } else {
+        start = max(thread_num * 2, 12);
+    }
+    // Previously on fails the backtracking would start from scratch
+    // Now we start from i/divisor
+    // 2 Seems to give faster result, went from 54s -> 42s
+    let divisor = matches
+        .value_of("Divisor")
+        .unwrap()
+        .parse()
+        .expect("Expected a number");
+    let increment = 2 * thread_num;
+    let mut threads = Vec::with_capacity(thread_num);
     // Spawn threads with explicit stack size
     // Needed because of the heavy recursion
-    for i in 0..THREAD_NUM {
+    for i in 0..thread_num {
         let builder = thread::Builder::new();
         threads.push(
             builder
-                .stack_size(STACK_SIZE)
-                .spawn(move || test_for_cycles(max, start, increment, i * 2, divisor))
+                .stack_size(stack_size)
+                .spawn(move || test_for_cycles(maximum, start, increment, i * 2, divisor))
                 .unwrap(),
         );
     }
